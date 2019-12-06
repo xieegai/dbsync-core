be4b6e3ede210ca597fcc835e440b57dd00dd9be
bailaohe <bailaohe@gmail.com>
2019-12-06 22:20:25 +0800

    updated

---
+++ src/main/java/com/github/bailaohe/dbsync/subscribe/DBSyncProcessor.java
@@ -0,0 +1,35 @@
+package com.github.bailaohe.dbsync.subscribe;
+
+import com.github.bailaohe.dbsync.event.payload.RowBatchChanged;
+import com.google.common.collect.ImmutableList;
+import lombok.AllArgsConstructor;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by baihe on 2017/8/24.
+ */
+@AllArgsConstructor
+public class DBSyncProcessor {
+
+    private DBSyncHandlerRegistry handlerRegistry;
+
+    public void processDBSync(String interest, RowBatchChanged payload) {
+        processDBSync(interest, payload, ImmutableList.of(), ImmutableList.of());
+    }
+
+    public void processDBSync(String interest, RowBatchChanged payload, List oldRows, List newRows) {
+
+        if (handlerRegistry.containsKey(interest)) {
+            List<IDBSyncHandler> handlers = handlerRegistry.get(interest);
+
+            if (CollectionUtils.isEmpty(oldRows) && CollectionUtils.isEmpty(newRows)) {
+                handlers.forEach(handler -> handler.onChange(payload));
+            } else {
+                handlers.forEach(handler -> handler.onChange(payload, oldRows, newRows));
+            }
+        }
+    }
+}
